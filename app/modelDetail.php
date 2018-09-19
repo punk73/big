243a3514ce4805de1da01cf203b81e03d56c8f4e
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-09-19 17:12:31 +0700

    changes encoding production number

--- app/modelDetail.php
+++ app/modelDetail.php
@@ -14,7 +14,7 @@ class modelDetail extends Model
     	// 'code'
     ];
 
-    public function generateCode($model_id){
+    public function generateCodeBackup($model_id){
     	// cek jumlah model detail dengan model id sperti berikut. 
         $modelDetailCount = $this->where('model_id', $model_id )->get();
         $modelDetailCount = count($modelDetailCount) + 1; //kalau null, maka no increment
@@ -23,6 +23,11 @@ class modelDetail extends Model
         //generate incrementing code
         $code = str_pad( dechex($modelDetailCount) , 3, '0', STR_PAD_LEFT );
         // assign code into model details
+        $this->code = $code;
+    }
+
+    public function generateCode($model_id){
+        $code = $this->prod_no;
         $this->code = $code;
     }
 }
