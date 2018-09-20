c9804374e1080e51c4421bb676a3e4ab04db61b8
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-09-20 10:02:03 +0700

    add foreign key to subtypes table

---
+++ app/Api/V1/Controllers/SubtypeController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Http\Controllers\Controller; //parent contoller
+use App\Subtype;
+use Illuminate\Http\Request;
+
+
+class SubtypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subtype  $subtype
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subtype $subtype)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subtype  $subtype
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subtype $subtype)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subtype  $subtype
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subtype $subtype)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subtype  $subtype
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subtype $subtype)
+    {
+        //
+    }
+}
--- database/migrations/2018_09_20_024623_create_table_subtypes.php
+++ database/migrations/2018_09_20_024623_create_table_subtypes.php
@@ -15,9 +15,13 @@ class CreateTableSubtypes extends Migration
     {
         Schema::create('subtypes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('model_id');
+            $table->integer('model_id')->unsigned();
             $table->string('name', 2 ); //it's actually only 1 char of string;
             $table->timestamps();
+
+            $table->foreign('model_id')
+              ->references('id')->on('models')
+              ->onDelete('cascade');
         });
     }
 
